c46883cf8ef2ffd33f96146afe873f030db61871
edwardyun12 <91218016+edwardyun12@users.noreply.github.com>
2026-02-22 08:01:30 +0000

    implemented basic upload page and added code for script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
-function showUploadFunction() {
-    TODO()
-}
+function showUploadSection() {
+        document.getElementById('home-section').style.display = 'none';
+        document.getElementById('upload-section').style.display = 'block';
+    }
+
+    function showHomeSection() {
+        document.getElementById('upload-section').style.display = 'none';
+        document.getElementById('home-section').style.display = 'block';
+    }
